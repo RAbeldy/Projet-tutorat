720a54650ec35c8fe8748781a3b76c77eba4fd10
Brice Nicodem <34067881+Brice2117@users.noreply.github.com>
2019-07-15 19:24:08 +0200

    Add files via upload

--- models/users.php
+++ models/users.php
@@ -80,7 +80,31 @@
     {
     $this->password=$pwd;
     }
+public function connexion($email, $pwd)
+    {
+        $db = Db::getInstance();
 
+        $request = $db->prepare('SELECT u.id_user as id from user as u, avoir_statut as av, statut_compte as Scpte  WHERE u.email=? and u.password=? and u.id_user = av.id_user and av.id_statut = Scpte.id_statut_compte and Scpte.libelle <> "ATTENTE_VALIDATION" ');
+        $request->execute(array($email,$pwd));
+        if ($request->rowCount() == 1)
+        {
+             echo('1 vous êtes connecté !');
+             $_SESSION['user_id']=$request->fetch()['id'];
+             
+             
+             //on recupere l'ID du statut de l'user
+             $request=$db->query('SELECT avst.id_statut as id_statut,us.nom as nom,us.prenom as prenom,us.email as mail from user as us, avoir_statut as avst WHERE  avst.id_user=us.id_user and avst.id_user = '.$_SESSION['user_id'].'');
+             $res = $request->fetch();
+
+             $_SESSION['mail']=$res['mail'];
+             $_SESSION['nom']=$res['nom'];
+             $_SESSION['prenom']=$res['prenom'];
+             $_SESSION['statut_id']=$res['id_statut'];
+
+        } else {
+            echo('0  echec de connexion !');
+        }  
+    }
  public  function Create_user()
      {
          $db = Db::getInstance();
