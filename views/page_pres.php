387dad6348901764615317f443bb1aa2efc19afd
juniortej <juntej72@gmail.com>
2019-08-07 00:12:56 +0200

    add function for scroll images in the page_pres

--- views/page_pres.php
+++ views/page_pres.php
@@ -1,19 +1,39 @@
 
     <div class="global_container">
-        <header class="block-intro">
-            <div class="jumbotron">
+         <div class="w3-content w3-section" style="background-size: auto; background-position: center; top">
+              <img class="mySlides" src="../../assets/img/accueil.jpg" style="width:100%">
+              <img class="mySlides" src="../../assets/img/1.jpg" style="width:100%">
+              <img class="mySlides" src="../../assets/img/2.jpg" style="width:100%">
+              <img class="mySlides" src="../../assets/img/3.jpg" style="width:100%">
+    </div>
+
+            <script>
+                var myIndex = 0;
+                carousel();
+
+                function carousel() {
+                  var i;
+                  var x = document.getElementsByClassName("mySlides");
+                  for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";  
+                  }
+                  myIndex++;
+                  if (myIndex > x.length) {myIndex = 1}    
+                  x[myIndex-1].style.display = "block";  
+                  setTimeout(carousel, 2000); // Change image every 2 seconds
+                }
+            </script>
+
+
+        <div class="container">
+            <section class="portfolio-block skills">
+                <div class="container">
+            <div class="heading">
                 <h1 id="h1bienvenue">BIENVENUE SUR TUTORAT YNCREA</h1>
                 <p id="middleJumboParagraph">Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor
                     sit amet&nbsp;Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet&nbsp;Lorem Ispum dolor sit amet<br><br></p>
                 <p id="callToAction" class="text-center"><a class="btn" role="button">En savoir plus</a></p>
             </div>
-        </header>
-        <div class="container">
-            <section class="portfolio-block skills">
-                <div class="container">
-                    <div class="heading">
-                        <h2>A PROPOS</h2>
-                    </div>
                     <div class="row">
                         <div class="col-md-4 space">
                             <div class="card special-skill-item border-0">
